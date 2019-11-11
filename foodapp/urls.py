b69e31f6de805a82731b82f2e5f36077b5d4fce9
Madhav Bansal <madhavbansal.cse18@itbhu.ac.in>
2019-11-11 15:14:16 +0530

    check

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -5,7 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')urlpa
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+hello= "hee"
 urlpatterns = [
 	path('',include('webapp.urls')),
     path('admin/', admin.site.urls),
